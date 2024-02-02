312e7811e98fe38331e15cbd6059912aa16a29fd
rzaratem <rzaratem@NTTD-50PJXD3>
2024-02-01 23:49:20 -0500

    Proyecto inicializacion 1 Febrero 2024

---
+++ src/test/java/com/nttdata/microservices/MisServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
